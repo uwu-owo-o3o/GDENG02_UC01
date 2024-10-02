026b711acb98fead078eeef0883512469a80a8e7
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-10-02 17:13:56 +0800

    Initial commit

---
+++ Source/GDENG02_UC01/GDENG02_UC01.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GDENG02_UC01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GDENG02_UC01, "GDENG02_UC01" );
