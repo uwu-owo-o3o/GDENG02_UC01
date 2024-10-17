64e7ec495fde2f587879dddab2dd83bbd7d634d8
rritsu <andrea_legaspi@dlsu.edu.ph>
2024-10-18 02:36:11 +0800

    shop progress

---
+++ Source/GDENG02_UC01/ShopManager.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerProgressManager.h"
+#include "ShopManager.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GDENG02_UC01_API UShopManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UShopManager();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void CheckBuyWorker();
+
+public:
+	UPROPERTY(EDitAnywhere)
+	bool buyWorker;
+
+	UPROPERTY(EDitAnywhere)
+	AActor* PlayerProgressManagerActor;
+
+
+};
